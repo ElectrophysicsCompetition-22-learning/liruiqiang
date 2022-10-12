ebe4bded33edf38ed9f327107d7811810abd5291
1 <1.com>
2022-10-12 16:34:46 +0800

    1

---
+++ 未命名1.c
@@ -0,0 +1,34 @@
+#include<stdio.h> 
+/*int main()
+{
+	int x=5;
+	int y=10;
+	printf("%d\n",x++);
+	printf("%d\n",++y);
+	return 0;
+}
+#include<string.h>
+int main()
+{
+	char ch[]="hello";
+	int len=strlen(ch);
+	printf("×Ö·û´®³¤¶ÈÎª%d\n",len);
+	return 0;
+}
+int main()
+{
+	char a='B',b='o',c='k';
+	putchar(a);putchar(b);putchar(b);putchar(c);putchar('\t');
+	putchar(a);putchar(b);
+	putchar('\n');
+	putchar(b);putchar(c); 
+	return 0;
+}*/
+int main()
+{
+	char c;
+	printf("input a character\n");
+	c=getchar();
+	putchar(c);
+	return 0;
+}
---
+++ 未命名2.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+/*int main()
+{
+	int a=0,b=0,c=0,d=0;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("a+b=%d\n",a+b);
+	scanf("%d %d",&c,&d);
+	printf("c*d=%d\n",c*d);
+	return 0;
+}
+#define PRICE 10
+int main()
+{
+	int total,num;
+	num=5;
+	total=num*PRICE;
+	printf("%d",total);
+	return 0;
+} 
+int main()
+{
+	float height;
+	height=172.5;
+	printf("%f\n",height);
+	return 0;
+}
+#define PI 3.14
+int main()
+{
+	int r;
+	float s;
+	r=2;
+	s=PI*r*r;
+	printf("s=%.2f",s);
+	return 0;
+}
+int main()
+{
+	char word,password;
+	printf("请输入a~n的一个字母:");
+	scanf("%c",&word);
+	password=word+1;
+	printf("加密后的字母为%c\n",password);
+	return 0; 
+}
+int main()
+{
+	int n,a1,a2,a3;
+	printf("请输入三位整数：");
+	scanf("%d",&n);
+	a1=n/100;
+	a2=n/10%10;
+	a3=n%10;
+	printf("%d%d%d\n",a3,a2,a1);
+	return 0; 
+}
+int main()
+{
+	int x=5;
+	int y=10;
+	printf("%\n",x++);
+	printf("%d\n",++y);
+	return 0;
+}*/
+int main()
+{
+	int age;
+	age=18;
+	double height;
+	height=160.5;
+	double weight;
+	weight=90.5;
+	printf("性别：男\n");
+	printf("年龄：%d\n",age);
+	printf("身高：%lf\n",height);
+	printf("体重：%lf\n",weight);
+	return 0;
+}
+
+
+	
+
